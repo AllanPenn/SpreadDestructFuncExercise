6c156d71ef6108611c7e3ec5ebc86c8a85a5d93a
Allan Pennycuff <allanp1238@gmail.com>
2021-10-21 08:14:04 -0500

    Completed initial version of exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+const jPDinos = [
+  "Velociraptors",
+  "Tyrannosaurus Rex",
+  "Dilophosaurus"
+];
+function seeDinos(a, ...b){
+  console.log(a);
+  console.log(b);
+}
+seeDinos(...jPDinos);
+const jPCharacters = {
+  alanGrant: "Sam Neill",
+  ellieSattler: "Laura Dern",
+  ianMalcolm: "Jeff Goldblum"
+};
+seeCharacters = ({ellieSattler}) => {
+  console.log(ellieSattler);
+}
+seeCharacters(jPCharacters);
